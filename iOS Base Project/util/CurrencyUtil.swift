c23bfa2b7650c670b0a4fbbbf443fba56d7ffb0b
lafran <lafran@docotel.co.id>
2018-01-24 10:12:04 +0700

    Update CurrencyUtil.swift

--- iOS Base Project/util/CurrencyUtil.swift
+++ iOS Base Project/util/CurrencyUtil.swift
@@ -1,25 +1,25 @@
 //
-// Created by Lafran Pane on 1/16/18.
-// Copyright (c) 2018 Docotel Group. All rights reserved.
+// Created by Lafran Pane on 1/24/18.
+// Copyright (c) 2018 Docotel. All rights reserved.
 //
 
 import Foundation
 
 open class CurrencyUtil {
 
-    open class func toRpFormat(_ value: Double) -> String? {
-        let formatter = rupiahFormatter
-        return formatter.string(from: NSNumber(value: value))
+    open class func toRpFormat(_ value: Double) -> String {
+        let amount = rupiahFormatter.string(from: NSNumber(value: value))!
+        return addPaddingSpace(from: amount)
     }
 
-    open class func toRpFormat(_ value: Float) -> String? {
-        let formatter = rupiahFormatter
-        return formatter.string(from: NSNumber(value: value))
+    open class func toRpFormat(_ value: Float) -> String {
+        let amount = rupiahFormatter.string(from: NSNumber(value: value))!
+        return addPaddingSpace(from: amount)
     }
 
-    open class func toRpFormat(_ value: Int) -> String? {
-        let formatter = rupiahFormatter
-        return formatter.string(from: NSNumber(value: value))
+    open class func toRpFormat(_ value: Int) -> String {
+        let amount = rupiahFormatter.string(from: NSNumber(value: value))!
+        return addPaddingSpace(from: amount)
     }
 
     open class func clearRp(_ source: String) -> String {
@@ -27,11 +27,19 @@ open class CurrencyUtil {
         return replaced
     }
 
+    open class func addPaddingSpace(from value: String) -> String {
+        var mutableStr = value
+        let symbol = rupiahFormatter.currencySymbol!
+        mutableStr.insert(" ", at: symbol.index(symbol.startIndex, offsetBy: symbol.count))
+        return mutableStr
+    }
+
 }
 
 public var rupiahFormatter: NumberFormatter {
     let formatter = NumberFormatter()
     formatter.numberStyle = .currency
-    formatter.locale = Locale(identifier: "id")
+    formatter.locale = Locale(identifier: "in_ID")
     return formatter
 }
+
